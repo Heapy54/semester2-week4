a20c4a99b9030774f1506aa456b38aabb03653b5
Daniel Heap <tldl0545@leeds.ac.uk>
2026-02-18 13:24:55 +0000

    portfolio

--- worksheet/portfolio/makeString.c
+++ worksheet/portfolio/makeString.c
@@ -12,15 +12,16 @@ int main( int argc, char **argv ) {
     // define appropriate data to hold your answer
     char buffer[1024]="";
 
+    
+    // process the command-line data using appropriate string functions
+    
     for(int i = 1; i<argc; i++){
         strcat(buffer, argv[i]);
         if(i < argc -1){
             strcat(buffer, "-");
         }
     }
-
-    // process the command-line data using appropriate string functions
-
+    
     printf("%s\n",buffer); // use only this single print statement in your submitted code
 
     return 0;
